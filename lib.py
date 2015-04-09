7c360e51a6dceaa7ebc1067bbaee3cdbbddcc6c1
berlinHsin <berlin.hsin@gmail.com>
2015-04-10 00:10:27 +0800

    Combine encode and decode

--- lib.py
+++ lib.py
@@ -2,36 +2,30 @@ import string , math
 
 dictionary = list(string.ascii_lowercase)
 
-def decoder(key , decode):
+def de_en_coder(key,token,state):
 	result = list()
-	key_limit = len(key) - 1
-	key_ptr = 0
-	for d in decode :
-		d_index = dictionary.index(d)
-		k_index = dictionary.index(key[key_ptr])
-		result.append(dictionary[ (d_index-k_index)%26 ])
-		key_ptr += (key_ptr==key_limit) and -key_ptr or 1
-	return "".join(result)
-
-
-def encoder(key , encode):
-	result = list()
+	token_lower = token.lower()
 	key_limit = len(key) - 1
 	key_ptr = 0 
-	for e in encode :
-		if e not in dictionary :
-			result.append(e)
+	for i,t in enumerate(token_lower) :
+		if t not in dictionary :
+			result.append(t)
 			continue
-		e_index = dictionary.index(e)
+		t_index = dictionary.index(t)
 		k_index = dictionary.index(key[key_ptr])
-		result.append(dictionary[ (e_index+k_index)%26 ])
+		r_index = (state=='encode') and (t_index+k_index) or (t_index-k_index) 
+		r_index %= 26
+		if token[i].isupper():
+			result.append(dictionary[r_index].upper())
+		else:
+			result.append(dictionary[r_index])
 		key_ptr += (key_ptr==key_limit) and -key_ptr or 1
 	return "".join(result)
 
 if __name__ == "__main__" :
-	word = "berlin"
+	word = "Hello World!"
 	key  = "claire"
-	word_decoded = decoder(key,word)
-	word_encoded = encoder(key,word_decoded)
+	word_decoded = de_en_coder(key,word,'encode')
+	word_encoded = de_en_coder(key,word_decoded,'decode')
 	print word_decoded
 	print word_encoded
